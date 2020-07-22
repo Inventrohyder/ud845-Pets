179e2c5f026734d5128684a4b1e00cd562750cd3
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-07-23 02:07:58 +0300

    ✨ feat: handle unknown breed case

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -64,6 +65,8 @@ public class PetCursorAdapter extends CursorAdapter {
         // Read the pet attributes from the Cursor for the current pet
         String petName = cursor.getString(nameColumnIndex);
         String petBreed = cursor.getString(breedColumnIndex);
+
+        if (TextUtils.isEmpty(petBreed)) petBreed = context.getString(R.string.breed_unknown);
 
         // Update the TextViews with the attributes for the current pet
         name.setText(petName);
